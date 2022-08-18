288a55ceb0a38fb7d165307e86edd2d94ab75217
Bina2208 <83944511+Bina2208@users.noreply.github.com>
2022-08-18 11:07:51 +0200

    Add random messages for good work.

---
+++ main.js
@@ -0,0 +1,8 @@
+let objMessages = {
+    firstData: ["This is", "You are", "You look"],
+    secondData: ["amazing", "awesome", "great", "brilliant", "fantastic"],
+    thirdData: ["Well done.", "Good job.", "Way to go!" ],
+};
+
+
+
