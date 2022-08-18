0839a9ed89e180a9a4765227e76ac9b4ad24d325
Bina2208 <83944511+Bina2208@users.noreply.github.com>
2022-08-18 11:37:03 +0200

    Add randomize function and random message generation.

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 let objMessages = {
-    firstData: ["This is", "You are", "You look"],
-    secondData: ["amazing", "awesome", "great", "brilliant", "fantastic"],
-    thirdData: ["Well done.", "Good job.", "Way to go!" ],
+    firstData: ["This is", "You are", "You look", "Your team is", "Your work is"],
+    secondData: ["awesome", "great", "fantastic"],
+    thirdData: ["Well done", "Good job", "Way to go" ],
 };
 
+let funcRandomNum = (maxNum) => {
+    return Math.floor(Math.random() * (maxNum+1));
+}
 
+let funcRandomMessage = (objMessages) => {
+    let firstLength = objMessages.firstData.length - 1;
+    let secondLength = objMessages.secondData.length -1;
+    let thirdLength = objMessages.thirdData.length -1;
+    console.log(`${objMessages.firstData[funcRandomNum(firstLength)]} ${objMessages.secondData[funcRandomNum(secondLength)]}. ${objMessages.thirdData[funcRandomNum(thirdLength)]}!`);
+}
 
+funcRandomMessage(objMessages);
